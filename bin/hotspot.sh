0faa16700415b564d2110f6c66f83263706f0005
rpbritton <ryan@rbritton.dev>
2021-05-10 17:02:24 -0400

    hide hotspot password

--- bin/hotspot.sh
+++ bin/hotspot.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-if [[ $# == 0 ]]
+HOTSPOT_PASSWORD=$(cat $HOME/bin/secrets/hotspot_password.txt)
+
+if [[ $# != 1 ]]
 then
 	echo "Needs parameter 'on' or 'off'"
 	exit 1
@@ -8,7 +10,7 @@ fi
 
 if [[ $1 == "on" ]]
 then
-	nmcli device wifi hotspot ifname wlp1s0 ssid "Aspen" password "watermelon"
+	nmcli device wifi hotspot ifname wlp1s0 ssid "aspen" password $HOTSPOT_PASSWORD
 elif [[ $1 == "off" ]]
 then
 	nmcli connection down Hotspot
