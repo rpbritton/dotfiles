7daa6f2e55b5d7b78c02e23f09393579c7e74a4f
rpbritton <git@rbritton.dev>
2022-12-26 09:17:14 -0500

    Fix drive sync script

--- bin/drivebackup.sh
+++ bin/drivebackup.sh
@@ -1,28 +1,27 @@
 #!/bin/sh
 
-: '
-~/.config/systemd/user/autobackup.timer
-------------------------------------
-[Unit]
-Description=Run a backup every hour
-
-[Timer]
-OnCalendar=*-*-* *:00:00
-Persistent=true
-
-[Install]
-WantedBy=timers.target
-------------------------------------
-
-~/.config/systemd/user/autobackup.service
---------------------------------------
-[Unit]
-Description=Backup to google drive
-
-[Service]
-Type=oneshot
-ExecStart=/home/ryan/bin/drivebackup.sh
---------------------------------------
+# ~/.config/systemd/user/autobackup.timer
+# ------------------------------------
+# [Unit]
+# Description=Run a backup every hour
+#
+# [Timer]
+# OnCalendar=*-*-* *:00:00
+# Persistent=true
+#
+# [Install]
+# WantedBy=timers.target
+# ------------------------------------
+#
+# ~/.config/systemd/user/autobackup.service
+# --------------------------------------
+# [Unit]
+# Description=Backup to google drive
+#
+# [Service]
+# Type=oneshot
+# ExecStart=/home/ryan/bin/drivebackup.sh
+# --------------------------------------
 
 sudo systemctl enable autobackup.timer
 
@@ -34,43 +33,43 @@ declare -A accounts_to_sync
 
 if [[ $# -eq 0 ]]
 then
-	for account in "${accounts[@]}"
-	do
-		accounts_to_sync[$account]=
-	done
+    for account in "${accounts[@]}"
+    do
+        accounts_to_sync[$account]=
+    done
 fi
 
 while [[ $# -gt 0 ]]
 do
-	case $1 in
-		-a|--all)
-			for account in "${accounts[@]}"
-			do
-				accounts_to_sync[$account]=
-			done
-			shift 1
-			;;
-		*) 	accounts_to_sync[$1]=
-			shift 1
-			;;
-	esac
+    case $1 in
+        -a|--all)
+            for account in "${accounts[@]}"
+            do
+                accounts_to_sync[$account]=
+            done
+            shift 1
+        ;;
+        *) 	accounts_to_sync[$1]=
+            shift 1
+        ;;
+    esac
 done
 
 for account in "${!accounts_to_sync[@]}"
 do
-	case $account in
-		school)
-			(unbuffer grive -i $GRIVE_ID -e $GRIVE_SECRET -p "/home/ryan/Georgia Tech" | while read -t 1500 line; do echo $line; done; find "/home/ryan/Georgia Tech/.trash" -type f -mtime +30 -delete) &
-			;;
-		personal)
-			(unbuffer grive -i $GRIVE_ID -e $GRIVE_SECRET -p "/home/ryan/Google Drive" | while read -t 1500 line; do echo $line; done; find "/home/ryan/Google Drive/.trash" -type f -mtime +30 -delete) &
-			;;
-	esac
-
-	pids+=( $! )
+    case $account in
+        school)
+            (unbuffer grive -i $GRIVE_ID -e $GRIVE_SECRET -p "/home/ryan/Georgia Tech" | while read -t 1500 line; do echo $line; done; find "/home/ryan/Georgia Tech/.trash" -type f -mtime +30 -delete) &
+        ;;
+        personal)
+            (unbuffer grive -i $GRIVE_ID -e $GRIVE_SECRET -p "/home/ryan/Google Drive" | while read -t 1500 line; do echo $line; done; find "/home/ryan/Google Drive/.trash" -type f -mtime +30 -delete) &
+        ;;
+    esac
+    
+    pids+=( $! )
 done
 
 for pid in "${pids[@]}"
 do
-	wait $pid
+    wait $pid
 done
